3a06765b45bebbf6f451f119c572260ca19fd71a
matthewfertitta <MJFertitta@yahoo.com>
2024-11-17 14:21:38 -0600

    Initial commit

---
+++ src/main/java/net/matt/christmas/ChristmasClient.java
@@ -0,0 +1,11 @@
+package net.matt.christmas;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ChristmasClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/matt/christmas/ChristmasDataGenerator.java
@@ -0,0 +1,11 @@
+package net.matt.christmas;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ChristmasDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
